a4cc85059d1fd8a0a779fac50d04bc1c35469967
Satyaprakash-A <satyaprakash2006a@gmail.com>
2026-09-17 14:25:56 +0530

    Completed Task 2: Implemented search endpoint

--- src/main/java/com/skyscanner/HoenScannerApplication.java
+++ src/main/java/com/skyscanner/HoenScannerApplication.java
@@ -1,10 +1,17 @@
 package com.skyscanner;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.dropwizard.core.Application;
+import io.dropwizard.core.Configuration;
 import io.dropwizard.core.setup.Bootstrap;
 import io.dropwizard.core.setup.Environment;
 
-public class HoenScannerApplication extends Application<HoenScannerConfiguration> {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@SuppressWarnings("rawtypes")
+public class HoenScannerApplication extends Application {
 
     public static void main(final String[] args) throws Exception {
         new HoenScannerApplication().run(args);
@@ -16,13 +23,39 @@ public class HoenScannerApplication extends Application<HoenScannerConfiguration
     }
 
     @Override
-    public void initialize(final Bootstrap<HoenScannerConfiguration> bootstrap) {
+    public Class getConfigurationClass() {
+        return Configuration.class;
+    }
+
+    @Override
+    public void initialize(final Bootstrap bootstrap) {
 
     }
 
     @Override
-    public void run(final HoenScannerConfiguration configuration, final Environment environment) {
+    public void run(final Configuration configuration, final Environment environment) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
 
+        List carResults = Arrays.asList(
+                mapper.readValue(
+                        getClass().getClassLoader().getResource("rental_cars.json"),
+                        SearchResult[].class
+                )
+        );
+
+        List hotelResults = Arrays.asList(
+                mapper.readValue(
+                        getClass().getClassLoader().getResource("hotels.json"),
+                        SearchResult[].class
+                )
+        );
+
+        List searchResults = new ArrayList<>();
+        searchResults.addAll(carResults);
+        searchResults.addAll(hotelResults);
+
+        SearchResource resource = new SearchResource(searchResults);
+        environment.jersey().register(resource);
     }
 
 }
--- src/main/java/com/skyscanner/HoenScannerConfiguration.java
+++ src/main/java/com/skyscanner/HoenScannerConfiguration.java
@@ -3,5 +3,4 @@ package com.skyscanner;
 import io.dropwizard.core.Configuration;
 
 public class HoenScannerConfiguration extends Configuration {
-
 }
